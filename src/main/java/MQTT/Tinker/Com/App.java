f9d651c9b8c35fbe0ce74c08b3c76453ba070f92
Mark Evans <markleighevans@sky.com>
2016-03-20 18:52:18 +0000

    Initial commit

---
+++ src/main/java/MQTT/Tinker/Com/App.java
@@ -0,0 +1,13 @@
+package MQTT.Tinker.Com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
